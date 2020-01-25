6589594175c7211457633aef72b5b655d7e0977d
neymarjimoh <jemohkunle2007@gmail.com>
2020-01-25 01:07:44 +0100

    added

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(logger('dev'));
 // app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // Session 
 app.use(session({
